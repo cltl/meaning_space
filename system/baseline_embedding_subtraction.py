0ff4af683eb6b3ce6be6dc8e6977a76a41ac68df
Pia Sommerauer <piasommerauer@Pias-MacBook-Pro.local>
2018-02-23 16:16:39 +0100

    subtraction direction check

--- system/baseline_embedding_subtraction.py
+++ system/baseline_embedding_subtraction.py
@@ -8,7 +8,7 @@ import os
 def get_sim_subtracted(concept1, concept2, prop, model):
 
 
-    vec_sub = subtract(concept2, concept1, model)
+    vec_sub = subtract(concept1, concept2, model)
 
     if type(vec_sub) != str:
 
@@ -18,6 +18,18 @@ def get_sim_subtracted(concept1, concept2, prop, model):
 
     return sim_sub
 
+def get_sim_subtracted_rev(concept1, concept2, prop, model):
+
+
+    vec_sub = subtract(concept2, concept1, model)
+
+    if type(vec_sub) != str:
+
+        sim_sub = sim_wv(vec_sub, prop, model)
+    else:
+        sim_sub = 0.0
+
+    return sim_sub
 
 
 def extract_features(triples, model):
@@ -30,6 +42,21 @@ def extract_features(triples, model):
         prop = triple[2]
         if check_vocab([concept1, concept2, prop], model):
             features.append([get_sim_subtracted(concept1, concept2, prop, model)])
+        else:
+            features.append([0.0])
+
+    return features
+
+def extract_features_rev(triples, model):
+
+    features = []
+
+    for triple in triples:
+        concept1 = triple[0]
+        concept2 = triple[1]
+        prop = triple[2]
+        if check_vocab([concept1, concept2, prop], model):
+            features.append([get_sim_subtracted_rev(concept1, concept2, prop, model)])
         else:
             features.append([0.0])
 
@@ -48,6 +75,32 @@ def embedding_subtraction_classification_baseline(data_train, data_test, model, 
 
     x_train = extract_features(triples_train, model)
     x_test = extract_features(triples_test, model)
+
+
+
+    x_train_scaled, x_test_scaled = scale_training_test_features(x_train, x_test)
+
+    classifier = train_classifier(x_train_scaled, labels_train, hidden_layer, activation)
+
+    predictions = test_classifier(classifier, x_train_scaled)
+
+    results_to_file(triples_test, predictions, name)
+
+def embedding_subtraction_classification_baseline_rev(data_train, data_test, model, hidden_layer, activation):
+
+
+    hiddel_layer_str = '-'.join([str(l) for l in hidden_layer])
+
+    name = 'baseline_subtraction_rev_'+data_test
+
+    triples_train, labels_train = load_triples(data_train, return_labels = True)
+    triples_test = load_triples(data_test)
+
+    #x_train = extract_features(triples_train, model)
+    #x_test = extract_features(triples_test, model)
+
+    x_train = extract_features_rev(triples_train, model)
+    x_test = extract_features_rev(triples_test, model)
 
     x_train_scaled, x_test_scaled = scale_training_test_features(x_train, x_test)
 
@@ -77,3 +130,4 @@ if __name__ == '__main__':
 
 
     embedding_subtraction_classification_baseline(data_train, data_test, model, hidden_layer, activation)
+    embedding_subtraction_classification_baseline_rev(data_train, data_test, model, hidden_layer, activation)
--- system/full_def_subtraction_system.py
+++ system/full_def_subtraction_system.py
@@ -4,7 +4,7 @@ from embeddings import load_model
 from wordnet import get_all_definitions
 from baseline_wordnet import direct_def_check
 from definitions_extended_system import sim_def_check
-from baseline_embedding_subtraction import extract_features
+from baseline_embedding_subtraction import extract_features, extract_features_rev
 from classification import scale_training_test_features, train_classifier
 from classification import test_classifier
 import os
@@ -76,6 +76,72 @@ def full_def_embedding_sub_system(data_train, data_test, threshold1, threshold2,
     results_to_file(triples_test, answers, name)
     decisions_to_file(triples_test, decision_dicts, name)
 
+def full_def_embedding_sub_system_rev(data_train, data_test, threshold1, threshold2, model):
+
+
+    triples_train, labels_train = load_triples(data_train, return_labels = True)
+    triples_test = load_triples(data_test)
+
+    answers = []
+    decision_dicts = []
+    name = 'full_def_embedding_subtraction_system_rev_'+str(threshold1)+'-'+str(threshold2)+'_'+data_test
+
+    hidden_layer = (1,1)
+    activation = 'logistic'
+
+    x_train = extract_features_rev(triples_train, model)
+    x_test = extract_features_rev(triples_test, model)
+
+    x_train_scaled, x_test_scaled = scale_training_test_features(x_train, x_test)
+
+    print('training classifier...')
+    classifier = train_classifier(x_train_scaled, labels_train, hidden_layer, activation)
+    print('making predictions...')
+    predictions = test_classifier(classifier, x_test_scaled)
+
+
+    print('classifying triples')
+
+    total = len(triples_test)
+
+    for n, (triple, prediction) in enumerate(zip(triples_test, predictions)):
+        concept1 = triple[0]
+        concept2 = triple[1]
+        prop = triple[2]
+
+        def_answer = None
+        def_sim_answer = None
+        sim_answer = None
+
+        def_decision_dict = direct_def_check(concept1, concept2, prop)
+        def_sim_decision_dict = sim_def_check(concept1, concept2, prop, threshold1, threshold2, model)
+        def_answer = def_decision_dict['answer']
+        def_sim_answer = def_sim_decision_dict['answer']
+
+
+        if def_answer != None:
+            answers.append(def_answer)
+            decision_dicts.append(def_decision_dict)
+        elif def_sim_answer != None:
+            answers.append(def_sim_answer)
+            decision_dicts.append(def_sim_decision_dict)
+        else:
+            sub_decision_dict = dict()
+            sub_decision_dict['system'] = 'sub'
+            sub_decision_dict['answer'] = str(prediction)
+            sub_answer = str(prediction)
+            answers.append(sub_answer)
+            decision_dicts.append(sub_decision_dict)
+        status = n/total
+        if n in range(0, total, 10):
+            print(status, ' of the test data classified ')
+
+    print('writing results to file')
+    print('len data ', len(triples_test))
+    print('len answers ', len(answers))
+    results_to_file(triples_test, answers, name)
+    decisions_to_file(triples_test, decision_dicts, name)
+
 
 if __name__ == '__main__':
 
@@ -96,3 +162,4 @@ if __name__ == '__main__':
     threshold2 = 0.23
 
     full_def_embedding_sub_system(data_train, data_test, threshold1, threshold2, model)
+    #full_def_embedding_sub_system_rev(data_train, data_test, threshold1, threshold2, model)
